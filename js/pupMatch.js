cc6d0d332bbf6bcc2bcbc51838fb134815724ddf
Philip Nguyen <phi.nguyen@laverne.edu>
2017-02-20 13:16:44 -0800

    Fix mobile layout layout

--- js/pupMatch.js
+++ js/pupMatch.js
@@ -1,15 +1,18 @@
 var main = function () {
 
-  var $showNavBtn = $(".hamburger-btn");
+  // var $showNavBtn = $(".hamburger-btn");
   var $primaryNav = $(".primary-nav");
-  $showNavBtn.on("click", function() {
+  var $hamburgerWrapper = $(".hamburger-wrapper");
+  var $menuTxt = $(".menu-txt");
+
+  $hamburgerWrapper.on("click", function() {
     console.log('click');
     $primaryNav.slideToggle("normal", function() {
       if ($primaryNav.is(":visible") ) {
-          $showNavBtn.text("Hide Menu");
+          $menuTxt.text("Hide Menu");
       }
       else {
-        $showNavBtn.text("Show Menu");
+        $menuTxt.text("Show Menu");
       }
     });
   });
