92a454b77ef4b1cc0dc00b75bee8f06955f95b57
Philip Nguyen <phi.nguyen@laverne.edu>
2017-02-20 11:48:53 -0800

    Add show and hide nav btn

--- js/pupMatch.js
+++ js/pupMatch.js
@@ -1,6 +1,19 @@
 var main = function () {
-  "use strict";
-  //code
+
+  var $showNavBtn = $(".show-nav-btn");
+  var $primaryNav = $(".primary-nav");
+  $showNavBtn.on("click", function() {
+    console.log('click');
+    $primaryNav.slideToggle("normal", function() {
+      if ($primaryNav.is(":visible") ) {
+          $showNavBtn.text("Hide Navigation");
+      }
+      else {
+        $showNavBtn.text("Show Navigation");
+      }
+    });
+  });
+
 };
 
 $(document).ready(main);
