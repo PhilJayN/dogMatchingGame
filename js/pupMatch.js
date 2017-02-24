87b3d4d5a885ed6f6547138efd3af43851eed47f
Philip Nguyen <phi.nguyen@laverne.edu>
2017-02-23 20:19:39 -0800

    Add removeclass and animate to a matched pair

--- js/pupMatch.js
+++ js/pupMatch.js
@@ -23,6 +23,7 @@ var app = {
     cards: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
     init: function() {
         this.shuffle();
+        console.log('jQuery ', $('.hello').length);
     },
 
 
@@ -64,24 +65,67 @@ var app = {
     },
 
     assignCards: function() {
-      $('.card').each(function(index){
-        // console.log('this', this);
-        // console.log('each fxn index', index);
-        console.log('test', app.cards[0]); //do not use this.cards[]. it refers
-        //to the div class of card.
-        $(this).attr('data-card-value', app.cards[index]);
-      });
-      this.clickHandlers();
+        $('.card').each(function(index) {
+            // console.log('this', this);
+            // console.log('each fxn index', index);
+            // console.log('test', app.cards[0]); //do not use this.cards[]. it refers
+            //to the div class of card.
+            $(this).attr('data-card-value', app.cards[index]);
+        });
+        this.clickHandlers();
     },
 
     clickHandlers: function() {
-      $('.card').on('click', function(){
-        console.log('click');
-          $(this).html('<p> hello</p>');
-      });
+        $('.card').on('click', function() {
+            console.log('click');
+            $(this).html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
+            app.checkMatch();
+        });
+    },
+
+    checkMatch: function() {
+      console.log('checkMatch running');
+      console.log('true or false',$('.selected').length == 2 );
+        if ($('.selected').length == 2) {
+          // console.log('testing match', $('.selected').first().data('cardValue') == $('.selected').last().data('cardValue'));
+            if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
+                $('.selected').each(function() {
+                  $(this).animate({
+                    opacity: 0
+                  }).removeClass('unmatched');
+                });
+                $('.selected').each(function() {
+                    $(this).removeClass('selected');
+                });
+            }
+        }
     }
 
 };
+
+//
+// checkMatch: function() {
+//      if ($('.selected').length === 2) {
+//        if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
+//          $('.selected').each(function() {
+//            $(this).animate({
+//              opacity: 0
+//            }).removeClass('unmatched');
+//          });
+//          $('.selected').each(function() {
+//            $(this).removeClass('selected');
+//          });
+//          app.checkWin();
+//        } else {
+//          setTimeout(function() {
+//            $('.selected').each(function() {
+//              $(this).html('').removeClass('selected');
+//            });
+//          }, 1000);
+//        }
+//      }
+//    },
+
 
 
 // console.log(app.init());
